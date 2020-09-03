90c7df43e66b25e85f4bc46afa786246548b907c
Juslin Jukka <jusju@haaga-helia.fi>
2020-09-03 15:40:05 +0300

    First commit

---
+++ src/main/java/com/example/bookstorefall2020/BookstoreFall2020Application.java
@@ -0,0 +1,13 @@
+package com.example.bookstorefall2020;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoreFall2020Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoreFall2020Application.class, args);
+	}
+
+}
