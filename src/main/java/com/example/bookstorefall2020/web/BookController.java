7d688c066639e4140e0cb9170e2e88455c001ce2
Juslin Jukka <jusju@haaga-helia.fi>
2020-09-14 11:56:10 +0300

    added list endpoint to controller

--- src/main/java/com/example/bookstorefall2020/web/BookController.java
+++ src/main/java/com/example/bookstorefall2020/web/BookController.java
@@ -2,6 +2,7 @@ package com.example.bookstorefall2020.web;
 
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,16 +10,21 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstorefall2020.model.BookRepository;
+
 
 
 @Controller
 public class BookController {
+    
+	@Autowired
+	private BookRepository repository;
 
-    @RequestMapping(value="/index", method=RequestMethod.GET)
-    public String greetingForm(Model model) {
+	
+    @RequestMapping(value="/booklist")
+    public String bookList(Model model) {	
+        model.addAttribute("books", repository.findAll());
         return "booklist";
     }
-
-
 
 }
