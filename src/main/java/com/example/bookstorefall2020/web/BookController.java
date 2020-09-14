05d1c0ddceba6a48cfe6bde1967f6992bbeda26f
Juslin Jukka <jusju@haaga-helia.fi>
2020-09-14 12:09:49 +0300

    added controller endpoints for add and delete. thymeleaf for adding

--- src/main/java/com/example/bookstorefall2020/web/BookController.java
+++ src/main/java/com/example/bookstorefall2020/web/BookController.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.bookstorefall2020.model.Book;
 import com.example.bookstorefall2020.model.BookRepository;
 
 
@@ -26,5 +28,23 @@ public class BookController {
         model.addAttribute("books", repository.findAll());
         return "booklist";
     }
+    
+    @RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }  
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }  
+    @RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:booklist";
+    }    
+    
+    
 
 }
