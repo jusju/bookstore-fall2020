c12888fc3e74dc0ab7d30bafd70db262b69fba42
Juslin Jukka <jusju@haaga-helia.fi>
2020-09-14 11:59:09 +0300

    working version of booklist

--- src/main/java/com/example/bookstorefall2020/model/Book.java
+++ src/main/java/com/example/bookstorefall2020/model/Book.java
@@ -22,40 +22,59 @@ public class Book {
 	
 	public Book(String title, String author, int year, String isbn, double price) {
 		super();
-		this.id = id;
 		this.title = title;
 		this.author = author;
 		this.year = year;
 		this.isbn = isbn;
 		this.price = price;
 	}
+
+	
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public String getAuthor() {
 		return author;
 	}
+
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+
 	public int getYear() {
 		return year;
 	}
+
 	public void setYear(int year) {
 		this.year = year;
 	}
+
 	public String getIsbn() {
 		return isbn;
 	}
+
 	public void setIsbn(String isbn) {
 		this.isbn = isbn;
 	}
+
 	public double getPrice() {
 		return price;
 	}
+
 	public void setPrice(double price) {
 		this.price = price;
 	}
